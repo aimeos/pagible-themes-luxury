b5ecdfb788d0d4d7cd6db4dc953cf10b3fa1e598
aimeos <aimeos@aimeos.org>
2026-09-14 16:20:24 +0000

    Added website config element for storing name of the website (#312)

--- database/seeders/LuxuryDemo.php
+++ database/seeders/LuxuryDemo.php
@@ -750,6 +750,7 @@ class LuxuryDemo extends AbstractDemo
         $logoId = $this->logoFile();
 
         $config = [
+            'website' => Validation::entry( 'website', ['title' => 'Avelune Retreat'], 'config' ),
             'logo' => [
                 'type' => 'logo',
                 'files' => [$logoId],
